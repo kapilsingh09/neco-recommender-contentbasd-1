32ff6d1c4aecb03fd6db8581ab990df4fc1d2de8
kapilsingh09 <karan404katyura@gmail.com>
2025-11-21 23:31:41 +0530

    requrements.txt added

--- server.py
+++ server.py
@@ -41,5 +41,7 @@ def home():
 def get_recommendations(anime_name: str, k: int = 20):
     return recommend(anime_name, k)
 
+
+# MY POOKIE server !!   
 if __name__ == "__main__":
     uvicorn.run("server:app", host="0.0.0.0", port=8000, reload=True)
